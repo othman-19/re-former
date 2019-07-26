aa0408ae41aa115e5e7946108056d2073dea9c81
Mesi <mse212002@gmail.com>
2019-07-26 14:54:55 +0300

    Form_for added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
   def new
-    
+    @user = User.new(params[:id])
   end
     
-  def create
+=begin 
+ def create
     @user = User.new(username: params[:username], email: params[:email], password: params[:password])
   
     if @user.save
@@ -11,19 +12,24 @@ class UsersController < ApplicationController
     else
       render :new
     end
+  end 
+=end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
+    end
   end
 
-  # def create
-  #   @user = User.new(user_params)
-  #   if @user.save
-  #     redirect_to new_user_path
-  #   else
-  #     render :new
-  #   end
-  # end
+  def edit
+    @user = User.find(params[:id])
+  end
 
-  # private
-  #   def user_params 
-  #     params.require(:user).permit(:username,:email,:password)
-  #   end
+  private
+    def user_params 
+      params.require(:user).permit(:username,:email,:password)
+    end
 end
