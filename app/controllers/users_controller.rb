c500dc83cbb0b17f0e26226788993b47b90fab59
othmane <othman-19@hotmail.fr>
2019-07-26 15:38:20 +0100

    display error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,18 +25,13 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find_by(id: params[:id])
+    @user = User.find(params[:id])
   end
-=begin 
- def update
-  @user = User.find(params[:id])
- end 
-=end
 
   def update
     @user = User.find(params[:id])
-    if @user.save
-      redirect_to edit_user_path(@user)
+    if @user.update(user_params)
+      redirect_to edit_user_path
     else
       render :edit
     end
