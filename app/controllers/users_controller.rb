0cfb4041c507087d290eb4e3b203e6b3faedd14f
othmane <othman-19@hotmail.fr>
2019-07-29 11:04:13 +0100

    add stickler errors fixings

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,7 @@
-# frozen_string_literal: true
-
 class UsersController < ApplicationController
   def new
     @user = User.new(params[:id])
   end
-
-  #
-  #  def create
-  #     @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-  #
-  #     if @user.save
-  #       redirect_to new_user_path
-  #     else
-  #       render :new
-  #     end
-  #   end
 
   def create
     @user = User.new(user_params)
@@ -36,11 +23,11 @@ class UsersController < ApplicationController
     else
       render :edit
     end
-  end
+  end 
+
 
   private
-
-  def user_params
-    params.require(:user).permit(:username, :email, :password)
-  end
+    def user_params 
+      params.require(:user).permit(:username,:email,:password)
+    end
 end
